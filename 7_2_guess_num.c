a3e3c507190e198b7378c5fe8cf1b4c80b411dc2
hao134 <hao134@github.com>
2021-11-23 15:17:41 +0800

    guess one number

---
+++ 7_2_guess_num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int answer = 4;
+    int guess;
+    printf("Please enter your guess: ");
+    scanf("%d", &guess);
+    if (guess > answer) {
+        printf("Too large!\n");
+    } else if (guess < answer) {
+        printf("Too small\n");
+    } else {
+        printf("Correct!\n");
+    }
+    return 0;
+}
