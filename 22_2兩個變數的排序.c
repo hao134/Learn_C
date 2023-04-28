0ecac972858e43f3d1a4a04422424cd7aaf7aa96
hao134 <hsh319ab@gmail.com>
2023-04-28 18:01:56 +0800

    22.2使用指標來交換數字並排序

---
+++ 22_2兩個變數的排序.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+void sort(int *, int *);
+void swap(int *, int *);
+
+int main() {
+    int a = 5, b = 3;
+    sort(&a, &b);
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+}
+
+void sort(int *a, int *b) {
+    if (*a > *b) {
+        swap(&*a, &*b); //swap(a,b)
+    }
+}
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
